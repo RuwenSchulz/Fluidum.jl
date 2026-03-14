d1482955ff4a30042bc5dcd75250191c363b4fd3
Ruwen Schulz <schulz_r@tphys.uni-heidelberg.de>
2026-03-14 13:18:57 +0100

    WIP: local changes before merging upstream

--- src/Fluidum.jl
+++ src/Fluidum.jl
@@ -100,6 +100,8 @@ include("Matrix/1d_ideal_HQ_cilindrical_fugacity.jl")
 include("Matrix/1d_ideal_HQ_cilindrical_p_br_density.jl")
 include("Matrix/1d_ideal_HQ_cilindrical_p_br_fugacity.jl")
 include("Matrix/HQ_const_BG.jl")
+include("Matrix/HQ_const_BG_2nd_moment.jl")
+
 include("Matrix/1d_viscous_cilindrical.jl")
 
 include("Matrix/2d_viscous.jl")
